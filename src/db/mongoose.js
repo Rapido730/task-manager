2fc06ab8322b918d6b412cb7afdb51928fb0b1da
Rapido730 <sharmamit510@gmail.com>
2022-12-26 18:59:27 +0530

    hosting issue resolved

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 // const validator = require ('validator')
 
 const url = process.env.DB_URL;
-// console.log(url);
 mongoose.connect(url, {
   //  connecting to database
   useNewUrlParser: true,
