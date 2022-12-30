d686f93d4b96b2a0cd25f4311ae99f7de34faf0c
Rapido730 <sharmamit510@gmail.com>
2022-12-30 16:54:04 +0530

    added cors policy

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
-const express = require('express')   // loading express
-require('./db/mongoose')   // we dont require anything this is just to run mongoose file
+const express = require("express"); // loading express
+require("./db/mongoose"); // we dont require anything this is just to run mongoose file
+const cors = require("cors");
+const User = require("./model/user");
+const Task = require("./model/task");
+const Note = require("./model/notes");
 
-const User = require('./model/user')
-const Task = require('./model/task')
-const Note = require('./model/notes')
+const UserRouter = require("./routers/user");
+const TaskRouter = require("./routers/task");
+const NoteRouter = require("./routers/note");
+const { findById } = require("./model/user");
 
-const UserRouter = require('./routers/user')
-const TaskRouter = require('./routers/task')
-const NoteRouter = require('./routers/note')
-const { findById } = require('./model/user')
-
-const app = express()              //   creating new app
-const port = process.env.PORT   // creating a port
+const app = express(); //   creating new app
+const port = process.env.PORT || 4000; // creating a port
 
 // app.use((req,res,next) => {
 //     res.status(503).send('Server is under mentainence!')
 // })
 
+app.use(cors());
+app.use(express.json()); // state that json will be used to transfer data
+app.use(UserRouter);
+app.use(TaskRouter);
+app.use(NoteRouter);
 
-
-app.use(express.json())              // state that json will be used to transfer data
-app.use(UserRouter)
-app.use(TaskRouter)
-app.use(NoteRouter)
-
-const multer = require('multer')
+const multer = require("multer");
 // const upload = multer({
 //     dest : 'images',
 //     limit : {
@@ -34,7 +33,7 @@ const multer = require('multer')
 //         if(!file.originalname.match(/\.(doc|docx)$/)){
 //             return cb(new Error('please upload word document'))
 //         }
- 
+
 //         cb(undefined,true)
 //     }
 // })
@@ -43,16 +42,16 @@ const multer = require('multer')
 //     res.send(200)
 // })
 
-
-app.listen(port, () => {        // instructing app that listen to port given
-    console.log('Server is up on port ' + port)
-})
+app.listen(port, () => {
+  // instructing app that listen to port given
+  console.log("Server is up on port " + port);
+});
 
 // const bcrypt = require('bcryptjs')
 
 // const Myfunction = async() => {
 //     const password = 'Red@123'
-//     const hashedPassword = await bcrypt.hash(password,8) 
+//     const hashedPassword = await bcrypt.hash(password,8)
 
 //     console.log(password)
 //     console.log(hashedPassword)
@@ -64,7 +63,7 @@ app.listen(port, () => {        // instructing app that listen to port given
 // Myfunction()
 
 // const jwt = require('jsonwebtoken')
-  
+
 // const Myfunction = async () => {
 //     const token = jwt.sign({ id : '1234'}, 'mynewlogin', {expiresIn : '7 days'})
 //     console.log(token)
@@ -79,13 +78,13 @@ app.listen(port, () => {        // instructing app that listen to port given
 // const main = async () => {
 //     // console.log('hello')
 //     // const task = await Task.findById('62a48a3488c49175a428cbca')
-   
+
 //     // await task.populate('author')
 //     // console.log(task.author)
 
 //     const user = await User.findById('62a48a1888c49175a428cbc4')
 //     await user.populate('tasks')
-//     console.log(user.tasks) 
+//     console.log(user.tasks)
 // }
 
-// main() 
+// main()
